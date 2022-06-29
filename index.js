70c68b57bd80c9712bf99b978c4a69e4997d795c
aidan <a.taylor.lynch@gmail.com>
2022-06-29 17:33:04 +1000

    recover wallet

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import fs from 'fs'
 import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
 import { DirectSecp256k1HdWallet, makeCosmoshubPath } from '@cosmjs/proto-signing'
+
+const pathToMnemonic = './words'
 
 const networkOptions = {
     bech32AddressPrefix: 'wasm',
@@ -9,9 +12,7 @@ const networkOptions = {
 
 const generateNewWallet = async (bech32AddressPrefix) => {
     const hdDerivationPath = makeCosmoshubPath(0)
-
     const numberOfWordsInMnemonic = 12
-    
     return await DirectSecp256k1HdWallet.generate(numberOfWordsInMnemonic, { hdPaths: [hdDerivationPath], prefix: bech32AddressPrefix })
 }
 
@@ -19,11 +20,19 @@ const createCosmosClient = async (url, bech32AddressPrefix, wallet) => {
     return await SigningCosmWasmClient.connectWithSigner(url, wallet, { prefix: bech32AddressPrefix })
 }
 
-const wallet = await generateNewWallet(networkOptions.bech32AddressPrefix)
+const getExistingWalletFromMnemonic = async (bech32AddressPrefix, mnemonic) => {
+    return await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, { prefix: bech32AddressPrefix})
+}
+
+//const wallet = await generateNewWallet(networkOptions.bech32AddressPrefix)
+const mnemonic = fs.readFileSync(pathToMnemonic).toString()
+const wallet = await getExistingWalletFromMnemonic(networkOptions.bech32AddressPrefix, mnemonic)
 const client = await createCosmosClient(networkOptions.url, networkOptions.bech32AddressPrefix, wallet)
 
 const [account] = await wallet.getAccounts()
 const accountBalance = await client.getBalance(account.address, networkOptions.feeToken)
+
+//fs.writeFileSync(pathToMnemonic, wallet.mnemonic)
 
 console.log(`
     address: ${account.address}
